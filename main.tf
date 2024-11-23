3a54763e5b869c42303fe7e4812d68f99b4f626d
Fabio Giaquinta <extern.fabio.giaquinta@europcar.com>
2024-11-23 16:17:33 +0100

    Adding terraform config to connect to mongodb

--- main.tf
+++ main.tf
@@ -9,18 +9,50 @@ terraform {
       name = "joblisting"
     }
   }
+
   required_providers {
     mongodbatlas = {
-      source  = "timgchile/mongodbatlas"
-      version = "1.3.8"
+      source = "mongodb/mongodbatlas"
+      version = "1.4.6"
     }
   }
 }
 
-# An example resource that does nothing.
-# Configure the MongoDB Atlas Provider for MongoDB Atlas for Government
 provider "mongodbatlas" {
-  public_key          = var.public_key
-  private_key         = var.private_key
+  public_key = var.public_key
+  private_key = var.private_key
 }
-# Create the resources
+
+resource "mongodbatlas_cluster" "db-cluster" {
+  project_id              = var.atlasprojectid
+  name                    = var.atlas_cluster_name
+
+  # Provider Settings "block"
+  provider_name = "TENANT" //free tier
+  backing_provider_name = "AWS"
+  provider_region_name = "EU_WEST_3" //free tier
+  provider_instance_size_name = "M0" //free tier
+}
+
+
+resource "mongodbatlas_database_user" "dbuser" {
+  username           = var.atlas_dbuser
+  password           = var.atlas_dbpassword
+  project_id         = var.atlasprojectid
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readWrite"
+    database_name = var.atlas_dbname
+  }
+
+}
+
+# resource "mongodbatlas_project_ip_access_list" "test" {
+#   project_id = var.atlasprojectid
+#   cidr_block = var.cidr
+# }
+
+output "db_cn_string" {
+  value = mongodbatlas_cluster.db-cluster.connection_strings.0.standard_srv
+}
--- variables.tf
+++ variables.tf
@@ -4,4 +4,19 @@ variable "public_key" {
 variable "private_key" {
   description = "The private API key for MongoDB Atlas"
 }
-
+variable "atlasprojectid" {
+  description = "Atlas Project ID"
+  default     = "5c98a80fc56c98ef210b8633"
+}
+variable "atlas_cluster_name" {
+  description = "Atlas Project ID"
+}
+variable "atlas_dbuser" {
+  description = "The db user for Atlas"
+}
+variable "atlas_dbpassword" {
+  description = "The db user passwd for Atlas"
+}
+variable "atlas_dbname" {
+  description = "The db name for Atlas"
+}
