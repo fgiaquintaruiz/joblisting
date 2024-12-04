c6982791a3bd48507fab0b7872899bd6c40ea385
Fabio Giaquinta <extern.fabio.giaquinta@europcar.com>
2024-12-04 13:27:33 +0100

    To build this archive, please enable the zip64 extension. #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
@@ -52,3 +54,5 @@ tasks.withType<Jar> {
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
     })
 }
+
+tasks.withType<ShadowJar> { isZip64 = true }
